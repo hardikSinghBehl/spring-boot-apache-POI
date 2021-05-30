a7169cb5859b80388bb737b09e849ab5bd4e188c
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-31 01:05:00 +0530

    added dummy project to read excel file data to database

---
+++ read-excel-to-database/src/main/java/com/hardik/kofta/ReadExcelToDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.kofta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadExcelToDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadExcelToDatabaseApplication.class, args);
+	}
+
+}
---
+++ read-excel-to-database/src/test/java/com/hardik/kofta/ReadExcelToDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.kofta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadExcelToDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
