af3c447696adfac9282f1b234ffce921c12ed111
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-30 19:53:34 +0530

    added dummy poc project for creating single excel from database records

---
+++ create-single-excel-from-database/src/main/java/com/hardik/killercroc/CreateSingleExcelFromDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.killercroc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateSingleExcelFromDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateSingleExcelFromDatabaseApplication.class, args);
+	}
+
+}
