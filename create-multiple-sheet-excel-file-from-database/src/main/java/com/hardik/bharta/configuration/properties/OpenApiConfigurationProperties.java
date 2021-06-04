729405403d4abc3b00a0565b192d230b42cd3986
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-04 19:33:06 +0530

    added Open-APi config classes

---
+++ create-multiple-sheet-excel-file-from-database/src/main/java/com/hardik/bharta/configuration/properties/OpenApiConfigurationProperties.java
@@ -0,0 +1,29 @@
+package com.hardik.bharta.configuration.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties(prefix = "com.hardik.bharta")
+public class OpenApiConfigurationProperties {
+
+	private Swagger swagger = new Swagger();
+
+	@Data
+	public class Swagger {
+		private String title;
+		private String description;
+		private String apiVersion;
+
+		private Contact contact = new Contact();
+
+		@Data
+		public class Contact {
+			private String email;
+			private String name;
+			private String url;
+		}
+	}
+
+}
