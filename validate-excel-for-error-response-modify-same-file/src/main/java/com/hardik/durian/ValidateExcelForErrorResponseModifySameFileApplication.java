5593240701658dbd580dd024bfa2fd4eadb29833
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-06 18:59:47 +0530

    added init project for validating POC in same file

---
+++ validate-excel-for-error-response-modify-same-file/src/main/java/com/hardik/durian/ValidateExcelForErrorResponseModifySameFileApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.durian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidateExcelForErrorResponseModifySameFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidateExcelForErrorResponseModifySameFileApplication.class, args);
+	}
+
+}
---
+++ validate-excel-for-error-response-modify-same-file/src/test/java/com/hardik/durian/ValidateExcelForErrorResponseModifySameFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.durian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidateExcelForErrorResponseModifySameFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
