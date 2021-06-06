33f9ac36bc284141de95af007e5ddd1660a4fd88
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-06 14:33:31 +0530

    added init project for validation POC

---
+++ validate-excel-for-error-list-response/src/main/java/com/hardik/pyaaru/ValidateExcelForErrorListResponseApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.pyaaru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidateExcelForErrorListResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidateExcelForErrorListResponseApplication.class, args);
+	}
+
+}
---
+++ validate-excel-for-error-list-response/src/test/java/com/hardik/pyaaru/ValidateExcelForErrorListResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.pyaaru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidateExcelForErrorListResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
