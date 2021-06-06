04fcd640879b742bc77ca8efe034a2bf51f67c7b
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-06 14:45:36 +0530

    added entity class

---
+++ validate-excel-for-error-list-response/src/main/java/com/hardik/pyaaru/entity/Employee.java
@@ -0,0 +1,47 @@
+package com.hardik.pyaaru.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee implements Serializable {
+
+	private static final long serialVersionUID = 291742406510888152L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", nullable = false, unique = true)
+	private UUID id;
+
+	@Column(name = "email_id", nullable = false, unique = true)
+	private String emailId;
+
+	@Column(name = "full_name", nullable = false)
+	private String fullName;
+
+	@Column(name = "salary_per_month", nullable = false)
+	private Double salaryPerMonth;
+
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt;
+
+	@PrePersist
+	void onCreate() {
+		this.id = UUID.randomUUID();
+		this.createdAt = LocalDateTime.now();
+	}
+
+}
