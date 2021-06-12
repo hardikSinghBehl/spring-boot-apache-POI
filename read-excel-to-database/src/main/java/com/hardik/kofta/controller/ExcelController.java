881ff98875165847020805de09d5dd0da40abfae
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-06-12 19:50:33 +0530

    added template download endpoint

--- read-excel-to-database/src/main/java/com/hardik/kofta/controller/ExcelController.java
+++ read-excel-to-database/src/main/java/com/hardik/kofta/controller/ExcelController.java
@@ -3,9 +3,12 @@ package com.hardik.kofta.controller;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +29,17 @@ import lombok.AllArgsConstructor;
 public class ExcelController {
 
 	private final ExcelService excelService;
+
+	@GetMapping(value = "/template")
+	@ResponseStatus(value = HttpStatus.OK)
+	@Operation(summary = "Download excel file template")
+	public ResponseEntity<InputStreamResource> excelTemplateFileDownloadHandler() throws IOException {
+		final var template = Thread.currentThread().getContextClassLoader().getResourceAsStream("template.xlsx");
+		return ResponseEntity.status(HttpStatus.OK)
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=template.xlsx")
+				.contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
+				.body(new InputStreamResource(template));
+	}
 
 	@IsXlsx
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
